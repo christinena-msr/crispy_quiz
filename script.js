6dfa5b3b9ab7dc811b0c1c71038d83311d2f128f
crispysodium <christinena.316@gmail.com>
2020-02-09 01:12:31 -0800

    added css & fixed local storage issues

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
     // user score variable
     let timeScore = timeLeft;
 
+
     // countdown
     let timeInterval = setInterval(function() {
         countdown.text("Time: " + timeLeft);
@@ -24,17 +25,31 @@ $(document).ready(function() {
         if (timeLeft === 0) {
             // user is out of time & stops quiz
             countdown.text("Time: 0");
+            // clear time
             clearInterval(timeInterval);
+            // clear quiz display
             quizArea.empty();
             setTimeout(countdown.text("Time's up!"), 1000);
+            const goBack = $("<button>");
+            goBack.attr("class", "btn btn-light");
+            goBack.attr("onclick", "location.href='./index.html';");
+            goBack.text("Go Back");
+            quizArea.append(goBack);
         } else if(index == 5) {
             // user finishes quiz & score recorded
             countdown.text(`Time: ${timeScore}`);
             clearInterval(timeInterval);
             quizArea.empty();
-            localStorage.setItem("score", timeScore);
+            const scoreBoard = JSON.parse(localStorage.getItem("score"));
+
+            let newScore = {
+                name: "",
+                score: timeScore
+            };
+            scoreBoard.push(newScore);
+            localStorage.setItem("score", JSON.stringify(scoreBoard));
             // user directed to score submit page
-            setTimeout(location.href = "./userScore.html", 1000);
+            setTimeout(location.href = "./userScore.html", 500);
         } 
     }, 1000);
 
@@ -51,21 +66,27 @@ $(document).ready(function() {
         for(let i=0; i<question.choices.length; i++) {
             let ansBtn = $("<button>");
             ansBtn.attr("id", `ans${i}`);
+            ansBtn.attr("class", "btn button-color ans");
             ansBtn.text(question.choices[i]);
             ansBox.append(ansBtn);
         }
+        // sets answer of the current quiz question
         let answer = question.answers;
-        $("button").on("click", function(event) {
+        // click event listener that check user's answer choice
+        $(".ans").on("click", function(event) {
             event.preventDefault();
             timeScore = timeLeft;
             if($(this).attr("id") === answer) {
-                feedback.text("correct!");
+                feedback.text("Correct!");
             } else {
-                feedback.text("wrong!");
+                feedback.text("Wrong!");
                 timeLeft = timeLeft - 5;
             }
+            setTimeout(() => {
+                feedback.text("")}, 700);
             index++;
-            if(index < 5) {
+            // checks if there are more questions
+            if(index < quiz.length) {
                 answer = "";
                 displayQ(index);
             } 
